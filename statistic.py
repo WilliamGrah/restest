92a5a09921d8feaf24a9d12ce29703b74ba4a5e7
William Grah Chequetto <contato@williamgrah.com>
2017-09-14 13:16:07 -0300

    Change string values to float
    Signed-off-by: William Grah Chequetto <contato@williamgrah.com>

--- statistic.py
+++ statistic.py
@@ -22,10 +22,33 @@ class DB:
         conn.close()
         return result
 
+
 class Statistic:
     def get(self):
         db = DB()
         result = db.get("""SELECT * FROM cpu_usage""")
+        result = self.prepare_data(result)
+
         return result
 
+    def prepare_data(self, tup):
+        result = []
+        for data in tup:
+            data = list(data)
+            data[2] = [float(x) if x.replace(".","").isdigit() else x for x in data[2]]
+            data[3] = self.parse_date(data[3])
+            result.append(data)
+            #for pos,value in enumerate(data[2]):
+            #    data[2][pos] = float(value) if value.replace(".","").isdigit() else value
+        return result
 
+    def parse_date(self, date):
+        date = "{}/{}/{} {}:{}:{}".format(
+                date.month,
+                date.day,
+                date.year,
+                date.hour,
+                date.minute,
+                date.second,
+                )
+        return date
